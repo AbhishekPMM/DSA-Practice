0eacbd8bc25c5a0586ef3e99007f3490d6a6838d
Abhishek P M <abhishekpm1333@gmail.com>
2026-05-25 23:51:13 +0530

    Solved 1 question

---
+++ longestconsecutive.py
@@ -0,0 +1,30 @@
+class Solution(object):
+
+    def longestConsecutive(self, nums):
+
+        s = set(nums)
+
+        longest = 0
+
+        for num in s:
+
+            # start of sequence
+            if num - 1 not in s:
+
+                length = 1
+
+                while num + length in s:
+                    length += 1
+
+                longest = max(longest, length)
+
+        return longest
+
+
+obj = Solution()
+
+nums = list(map(int, input().split()))
+
+result = obj.longestConsecutive(nums)
+
+print(result)
