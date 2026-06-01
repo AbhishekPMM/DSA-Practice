07d876d614ac8bf31250248b35c1f2e516259712
Abhishek P M <abhishekpm1333@gmail.com>
2026-06-01 19:45:14 +0530

    solved sliding window maximum using 2approches

---
+++ maxstring.py
@@ -0,0 +1,50 @@
+#approach 1
+'''class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        result=[]
+        left = 0
+        for right in range(len(nums)):
+            if right-left+1 == k:
+                result.append(max(nums[left:right+1]))
+                left += 1
+        return result
+obj = Solution()
+nums = list(map(int, input().split()))
+k=int(input())
+result = obj.maxSlidingWindow(nums,k)
+print(result)'''
+
+#approach 2
+from collections import deque
+
+class Solution(object):
+
+    def maxSlidingWindow(self, nums, k):
+
+        dq = deque()
+        result = []
+
+        for right in range(len(nums)):
+
+            while dq and nums[dq[-1]] < nums[right]:
+                dq.pop()
+
+            dq.append(right)
+
+            if dq[0] < right - k + 1:
+                dq.popleft()
+
+            if right >= k - 1:
+                result.append(nums[dq[0]])
+
+        return result
+
+
+obj = Solution()
+
+nums = list(map(int, input("Enter numbers: ").split()))
+k = int(input("Enter k: "))
+
+result = obj.maxSlidingWindow(nums, k)
+
+print(result)
---
+++ minstring.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def minstring(self,s,t):
+        if len(s)<len(t):
+            return ""
+        dt1 = {}
+        dt2 = {}
+        for i in t:
+            dt1[i] = dt1.get(i,0) + 1
+        result = [-1,-1]
+        have = 0
+        minlen = float('inf')
+        need = len(dt1)
+        left = 0
+        for right in range(len(s)):
+            c=s[right]
+            dt2[c] = dt2.get(c,0)+1
+            if c in dt1 and dt2[c] == dt1[c]:
+                have += 1
+            while(have == need):
+                if (right-left + 1) < minlen:
+                    minlen = right - left + 1
+                    result = [left,right]
+                dt2[s[left]] -= 1
+                if s[left] in dt1 and dt2[s[left]]<dt1[s[left]]:
+                    have -= 1
+                left += 1
+        l,r = result
+        if minlen == float('inf'):
+            return ""
+        return s[l:r+1]
+obj = Solution()
+s = input("enter the string : ")
+t = input("enter the string to check : ")
+result = obj.minstring(s,t)
+print(result)    
+            
+
+        
+
+        
+
